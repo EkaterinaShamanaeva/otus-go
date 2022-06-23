0ee8337b662d6860ef156186a0808eecab4bdfa0
Ekaterina Shamanaeva <ekaterina@MacBook-Air-Ekaterina.local>
2022-06-23 12:55:43 +0300

    First version: select

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "fmt"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,22 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	terminated := make(Bi)
+	go func() {
+		defer fmt.Println("Work exited.")
+		defer close(terminated)
+		i := 0
+		for i < len(stages) {
+			select {
+			case terminated <- stages[i](in):
+				fmt.Println(i)
+				in = terminated
+				i++
+			case <-done:
+				return
+			default:
+			}
+		}
+	}()
+	return terminated
 }
