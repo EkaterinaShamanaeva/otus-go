d0f5fe3425c897e1133e30d623778cdc228cff00
Ekaterina Shamanaeva <ekaterina@MacBook-Air-Ekaterina.local>
2022-06-28 11:03:00 +0300

    pipeline.go is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,57 +9,55 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	outChan := make(Bi)
+	// output channel
+	outputChan := make(Bi)
+	// channel to load data into the first stage
+	inputFirstStage := make(Bi)
 
-	inFirstStage := make(Bi)
-	var inNextStage In = inFirstStage
-	var outLastStage Out
-
+	// goroutine to load data from in into inputFirstStage with the ability
+	// to interrupt on an additional signal (done)
 	go func() {
-		for s := range in {
-			inFirstStage <- s
-		}
-		close(inFirstStage)
-	}()
-
-	for _, stage := range stages {
-		outLastStage = stage(inNextStage)
-		inNextStage = outLastStage
-	}
-
-	go func() {
-		//for s := range outLastStage {
-		//	outChan <- s
-		//}
-		//close(outChan)
-		//array := make([]interface{}, 0)
-		for s := range outLastStage {
+		defer close(inputFirstStage)
+		for element := range in {
 			select {
 			case <-done:
-				close(outChan)
-				//for {
-				//	_, ok := <-outChan
-				//	if !ok {
-				//		break
-				//	}
-				//}
+				return
+			case inputFirstStage <- element:
+			}
+		}
+	}()
+
+	var inputNextStage In = inputFirstStage
+	var outputLastStage Out
+
+	// loop to run stages
+	for _, stage := range stages {
+		outputLastStage = stage(inputNextStage)
+		inputNextStage = outputLastStage
+	}
+
+	// goroutine to load the result into outputChan with the ability
+	// to interrupt on an additional signal (done)
+	// (with priority case <-done)
+	go func() {
+		defer close(outputChan)
+		for {
+			select {
+			case <-done:
 				return
 			default:
 				select {
-				case outChan <- s:
+				case s, ok := <-outputLastStage:
+					if !ok {
+						return
+					}
+					outputChan <- s
 				case <-done:
 					return
 				}
 			}
-			//for x := range outChan {
-			//	array = append(array, x)
-			//}
-			//return
-
 		}
-		close(outChan)
-
 	}()
 
-	return outChan
+	return outputChan
 }
