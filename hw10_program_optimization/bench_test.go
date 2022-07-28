518a505d89f5acb7a9e819938c24a3761ba36505
Ekaterina Shamanaeva <ekaterina@MacBook-Air-Ekaterina.local>
2022-07-28 14:50:39 +0300

    HW10 is completed (changed benchmark)

--- hw10_program_optimization/bench_test.go
+++ hw10_program_optimization/bench_test.go
@@ -11,19 +11,26 @@ func BenchmarkStates(b *testing.B) {
 		b.Errorf("open zip error: %v", err)
 	}
 
+	defer func(r *zip.ReadCloser) {
+		errClose := r.Close()
+		if errClose != nil {
+			b.Errorf("close zip error: %v", errClose)
+		}
+	}(r)
+
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		b.StopTimer()
 		data, errF := r.File[0].Open()
 		if errF != nil {
 			b.Errorf("zip content open error: %v", errF)
 		}
+
+		b.StartTimer()
 		_, err = GetDomainStat(data, "biz")
+		b.StopTimer()
 		if err != nil {
 			b.Errorf("get users error: %v", err)
 		}
-	}
-
-	err = r.Close()
-	if err != nil {
-		b.Errorf("close zip error: %v", err)
 	}
 }
